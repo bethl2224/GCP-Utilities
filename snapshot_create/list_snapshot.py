2301839764eaad976553a037490ebb1d1c225e16
bethl2224 <bethanyliang2224@gmail.com>
2025-01-14 02:11:55 +0000

    simplified list snapshot workflow

--- snapshot_create/list_snapshot.py
+++ snapshot_create/list_snapshot.py
@@ -2,6 +2,7 @@
 
 
 # from google.cloud import compute_v1
+import logging
 from google.auth import default
 from googleapiclient.discovery import build
 import pprint as pp
@@ -9,6 +10,7 @@ import pprint as pp
 
 
 def list_snapshots(proj:str , filter:str):
+    """return the most recently created snapshots"""
     # client=compute_v1.SnapshotsClient()
     # #why do i need to specify project??
     # request = compute_v1.ListSnapshotsRequest(project=proj, filter=filter)
@@ -16,11 +18,23 @@ def list_snapshots(proj:str , filter:str):
     service = build('compute', 'v1', cache_discovery=False)
     request = service.snapshots().list(project=proj, filter=filter)
     page = request.execute()
-    print(page['items'])
-    for snapshot in page['items']:
-        res = [snapshot['name'], str(snapshot['diskSizeGb']), 
-              snapshot['creationTimestamp'], snapshot['sourceDisk'].split("/")[-1]]
-        pp.pprint(res)
+    if len(page['items'] == 0):
+        logging.warning(f"No snapshot with in {proj} with filter {filter}")
+        return
+    
+    
+    sorted_snapshots= sorted(page['items'], key=lambda x: x["creationTimestamp"], reverse=True)
+    return sorted_snapshots[-1]
+    
+    
+
+    
+    # for snapshot in page['items']:
+    #     # <class 'dict'>
+    #     print(type(snapshot))
+    #     res = [snapshot['name'], str(snapshot['diskSizeGb']), 
+    #           snapshot['creationTimestamp'], snapshot['sourceDisk'].split("/")[-1]]
+    #     pp.pprint(res)
         
    
         
