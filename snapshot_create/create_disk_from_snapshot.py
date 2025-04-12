3f29f638e3f8c83588375f773313d6624679b46f
bethl2224 <bethanyliang2224@gmail.com>
2025-04-12 01:19:50 +0000

    refactor snapshot code

--- snapshot_create/create_disk_from_snapshot.py
+++ snapshot_create/create_disk_from_snapshot.py
@@ -1,39 +1,11 @@
 #! /usr/bin/env python
 from __future__ import annotations
-import sys
-from typing import Any
-from google.api_core.extended_operation import ExtendedOperation
 import argparse
 from googleapiclient.discovery import build
-from google.auth import default
 from pprint import pprint as pp
+from utils import delete_disk_if_exists, wait_for_disk_creation
 import logging
 import yaml
-
-
-def wait_for_extended_operation(
-    operation: ExtendedOperation, verbose_name: str = "operation", timeout: int = 300
-) -> Any:
-    result = operation.result(timeout=timeout)
-    if operation.error_code:
-        print(
-            f"Error during {verbose_name}: [Code: {operation.error_code}]: {
-                operation.error_message}",
-            file=sys.stderr,
-            flush=True,
-        )
-        print(f"Operation ID: {operation.name}", file=sys.stderr, flush=True)
-        raise operation.exception() or RuntimeError(operation.error_message)
-
-    if operation.warnings:
-        print(f"Warnings during {verbose_name}:\n",
-              file=sys.stderr, flush=True)
-        for warning in operation.warnings:
-            print(f" - {warning.code}: {warning.message}",
-                  file=sys.stderr, flush=True)
-    return result
-
-# TODO: delete snapshot
 
 
 def create_disk_from_snapshot(
@@ -57,7 +29,7 @@ def create_disk_from_snapshot(
             "type": f"projects/{target_project_id}/zones/{target_zone}/diskTypes/{disk_type}",
         }
         logging.debug(
-            f"Creating disk {disk_name} in {target_zone} from snapshot {src_snapshot_name} in project {src_project_id}"
+            f"Creating disk {disk_name} in {target_zone} from snapshot {src_snapshot_name} in project {src_project_id} 🟨 "
         )
         # create disk
         request = service.disks().insert(project=target_project_id,
@@ -65,7 +37,7 @@ def create_disk_from_snapshot(
         # execute operation
         request.execute()
         logging.debug(
-            f"Disk {disk_name} created in {target_zone} from snapshot {src_snapshot_name} in project {src_project_id}"
+            f"Disk {disk_name} created in {target_zone} from snapshot {src_snapshot_name} in project {src_project_id} ✅"
         )
         return service.disks().get(project=target_project_id, zone=target_zone, disk=disk_name)
 
@@ -110,6 +82,11 @@ if __name__ == "__main__":
         else:
             print(
                 f"Disk: {disk_name} would be created from snapshot {src_snapshot_name} in project {target_project_id}.")
+            # delete_disk_if_exists(
+            #     project_id=src_project_id,
+            #     zone=target_zone,
+            #     disk_name=disk_name
+            # )
             create_disk_from_snapshot(
                 src_project_id=src_project_id,
                 target_zone=target_zone,
@@ -118,4 +95,12 @@ if __name__ == "__main__":
                 disk_size_gb=disk_size_gb,
                 target_project_id=target_project_id,
                 src_snapshot_name=src_snapshot_name
+            )
+
+            # wait for disk creation
+            wait_for_disk_creation(
+                project_id=target_project_id,
+                zone=target_zone,
+                disk_name=disk_name,
+                operation=None
             )
--- snapshot_create/create_snapshot.py
+++ snapshot_create/create_snapshot.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python
 
 
-
-
 from __future__ import annotations
 
 import logging
@@ -27,36 +25,10 @@ snapshot-1  10            us-central1-b/disks/my-vm-with-startup-script  READY
 """
 
 
-
 def wait_for_extended_operation(
     operation: ExtendedOperation, verbose_name: str = "operation", timeout: int = 300
 ) -> Any:
-    """
-    Waits for the extended (long-running) operation to complete.
 
-    If the operation is successful, it will return its result.
-    If the operation ends with an error, an exception will be raised.
-    If there were any warnings during the execution of the operation
-    they will be printed to sys.stderr.
-
-    Args:
-        operation: a long-running operation you want to wait on.
-        verbose_name: (optional) a more verbose name of the operation,
-            used only during error and warning reporting.
-        timeout: how long (in seconds) to wait for operation to finish.
-            If None, wait indefinitely.
-
-    Returns:
-        Whatever the operation.result() returns.
-
-    Raises:
-        This method will raise the exception received from `operation.exception()`
-        or RuntimeError if there is no exception set, but there is an `error_code`
-        set for the `operation`.
-
-        In case of an operation taking longer than `timeout` seconds to complete,
-        a `concurrent.futures.TimeoutError` will be raised.
-    """
     result = operation.result(timeout=timeout)
 
     if operation.error_code:
@@ -69,9 +41,11 @@ def wait_for_extended_operation(
         raise operation.exception() or RuntimeError(operation.error_message)
 
     if operation.warnings:
-        print(f"Warnings during {verbose_name}:\n", file=sys.stderr, flush=True)
+        print(f"Warnings during {verbose_name}:\n",
+              file=sys.stderr, flush=True)
         for warning in operation.warnings:
-            print(f" - {warning.code}: {warning.message}", file=sys.stderr, flush=True)
+            print(f" - {warning.code}: {warning.message}",
+                  file=sys.stderr, flush=True)
 
     return result
 
@@ -85,51 +59,26 @@ def wait_for_extended_operation(
 
 # 1. first find the disk is source project
 # 2. create a snapshot object, link .sourcedisk the it with the disk
-# 3. then use snapshotclient.insert the disk in 
+# 3. then use snapshotclient.insert the disk in
 
 def create_snapshot(
     project_id: str,
     disk_name: str,
     snapshot_name: str,
-    
+
     zone: str | None = None,
     region: str | None = None,
     location: str | None = None,
     disk_project_id: str | None = None,
 ) -> compute_v1.Snapshot:
-    
-    
-    """
-    Create a snapshot of a disk.
 
-    You need to pass `zone` or `region` parameter relevant to the disk you want to
-    snapshot, but not both. Pass `zone` parameter for zonal disks and `region` for
-    regional disks.
-
-    Args:
-        project_id: project ID or project number of the Cloud project you want
-            to use to store the snapshot.
-        disk_name: name of the disk you want to snapshot.
-        snapshot_name: name of the snapshot to be created.
-        zone: name of the zone in which is the disk you want to snapshot (for zonal disks).
-        region: name of the region in which is the disk you want to snapshot (for regional disks).
-        location: The Cloud Storage multi-region or the Cloud Storage region where you
-            want to store your snapshot.
-            You can specify only one storage location. Available locations:
-            https://cloud.google.com/storage/docs/locations#available-locations
-        disk_project_id: project ID or project number of the Cloud project that
-            hosts the disk you want to snapshot. If not provided, will look for
-            the disk in the `project_id` project.
-
-    Returns:
-        The new snapshot instance.
-    """
     if zone is None and region is None:
         raise RuntimeError(
             "You need to specify `zone` or `region` for this function to work."
         )
     if zone is not None and region is not None:
-        raise RuntimeError("You can't set both `zone` and `region` parameters.")
+        raise RuntimeError(
+            "You can't set both `zone` and `region` parameters.")
 
     if disk_project_id is None:
         disk_project_id = project_id
@@ -139,9 +88,10 @@ def create_snapshot(
         # disk client to query the disk client
         disk_client = compute_v1.DisksClient()
         # disk_client.get()
-        disk = disk_client.get(project=disk_project_id, zone=zone, disk=disk_name)
+        disk = disk_client.get(project=disk_project_id,
+                               zone=zone, disk=disk_name)
     else:
-    # get regional disk 
+        # get regional disk
         regio_disk_client = compute_v1.RegionDisksClient()
         disk = regio_disk_client.get(
             project=disk_project_id, region=region, disk=disk_name
@@ -154,34 +104,17 @@ def create_snapshot(
     # default as US?
     if location:
         snapshot.storage_locations = [location]
-    
+
     # compute_v1.SnapshotsClient()
     snapshot_client = compute_v1.SnapshotsClient()
-    
+
     logging.debug(f"Creating Snapshot to project ${project_id}")
-    operation = snapshot_client.insert(project=project_id, snapshot_resource=snapshot)
-
+    operation = snapshot_client.insert(
+        project=project_id, snapshot_resource=snapshot)
     wait_for_extended_operation(operation, "snapshot creation")
-
     return snapshot_client.get(project=project_id, snapshot=snapshot_name)
 
 
-# specify whether want to pass in zonal disk image or regional disk image
-
-# look at the disk project_id to create the snapshot, good!
-
-# create_snapshot(project_id, disk_name, snapshot_name, zone, region, location, disk_project_id)
-# create_snapshot("apt-gear-446423-v0", "my-vm-with-startup-script", "snapshot-2", "us-central1-b")
-
-
-#create snapshot in another project - it works
-
-create_snapshot("my-second-project-447013",  "my-vm-with-startup-script", "snapshot-2-copy-from-project-1", "us-central1-b", disk_project_id="apt-gear-446423-v0")
-
-# note need to enable compute engine api to use
-
-
-# able to create snapshot within same project or another project
-
-
-# TODO: move project from one to another
+# create snapshot in another project - it works
+create_snapshot("my-second-project-447013",  "my-vm-with-startup-script",
+                "snapshot-2-copy-from-project-1", "us-central1-b", disk_project_id="apt-gear-446423-v0")
